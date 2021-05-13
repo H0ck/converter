3341bfdef77036de125cbcf2d989e77c330835b6
Alejandro Guerrero <alexgue404@gmail.com>
2021-05-14 00:50:55 +0200

    chore: files

---
+++ index.js
@@ -0,0 +1,23 @@
+const oasToJob = require('./oasToJob');
+console.log("test")
+
+let args = process.argv.slice(2);
+
+let converterCommandMapping = {
+    'oasToJob': oasToJob
+}
+
+module.exports.oasToJob = oasToJob;
+
+if (args.concat.length === 0){
+    //Dynamic execution
+
+} else{
+    //Call the corresponding converter removing the converter name parameter.
+    if (converterCommandMapping[args[0]]){
+        converterCommandMapping[args[0]].convert(args.slice(1));
+    }
+    else {
+        console.log("Converter", args[0], "not found");
+    }
+}
--- oasToJob/index.js
+++ oasToJob/index.js
@@ -2,78 +2,79 @@ let fs = require('fs');
 const yaml = require('js-yaml');
 const utils = require('../utils');
 
-let args = process.argv.slice(2);
-
-console.log("Starting conversion")
-if (args.length < 1) {
-    console.log("Please specify oasFile to convert to a Job.")
-    return;
-}
-
-
-
-let oasFile;
-try {
-    oasFile = yaml.load(fs.readFileSync("./" + args[0], 'utf8'));
-} catch (err) {
-    console.error("Error loading file: ", args[0])
-    return;
-}
-
-//Do the conversion
-Object.keys(oasFile.scenarios).forEach(scenario => {
-    let jobResult = yaml.load(fs.readFileSync(__dirname + '/jobTemplate.yaml', 'utf8'));
-    jobResult.code = utils.JSONEscape(fs.readFileSync(__dirname + '/jobCode.js', 'utf8'));
-    oasFile.scenarios[scenario].users?.forEach(user => {
-        user.usage?.forEach(usage => {
-            if (Math.round(user.count * Object.values(usage)[0]) > 0) {
-                jobResult.parametrizationGroups.push({
-                    parameters: [
-                        {
-                            name: 'profile',
-                            type: 'list',
-                            definition: Object.keys(usage)[0]
-                        },
-                        {
-                            name: 'userType',
-                            type: 'list',
-                            definition: user.type
-                        },
-                        {
-                            name: 'collectionUrl',
-                            type: 'list',
-                            definition: oasFile.profiles[Object.keys(usage)[0]]?.url
-                        },
-                        {
-                            name: 'index',
-                            type: 'ranges',
-                            definition: "1-" + Math.round(user.count * Object.values(usage)[0])
-                        }
-                       
-                    ]
-                });
-            }
-
-        })
-    })
-
-    if (!fs.existsSync('./h0ck-output')) {
-        fs.mkdirSync('./h0ck-output');
+module.exports.convert = function convert(args) {
+    console.log("Starting conversion")
+    if (args.length < 1) {
+        console.log("Please specify oasFile to convert to a Job.")
+        return;
     }
-    fs.writeFile('./h0ck-output/outputJob-' + scenario + '.yaml', yaml.dump(jobResult), (err) => {
-        if (err) {
-            throw err;
+
+
+    let oasFile;
+    try {
+        oasFile = yaml.load(fs.readFileSync("./" + args[0], 'utf8'));
+    } catch (err) {
+        console.error("Error loading file: ", args[0])
+        return;
+    }
+
+    //Do the conversion
+    Object.keys(oasFile.scenarios).forEach(scenario => {
+        let jobResult = yaml.load(fs.readFileSync(__dirname + '/jobTemplate.yaml', 'utf8'));
+        jobResult.code = utils.JSONEscape(fs.readFileSync(__dirname + '/jobCode.js', 'utf8'));
+        oasFile.scenarios[scenario].users?.forEach(user => {
+            user.usage?.forEach(usage => {
+                if (Math.round(user.count * Object.values(usage)[0]) > 0) {
+                    jobResult.parametrizationGroups.push({
+                        parameters: [
+                            {
+                                name: 'profile',
+                                type: 'list',
+                                definition: Object.keys(usage)[0]
+                            },
+                            {
+                                name: 'userType',
+                                type: 'list',
+                                definition: user.type
+                            },
+                            {
+                                name: 'collectionUrl',
+                                type: 'list',
+                                definition: oasFile.profiles[Object.keys(usage)[0]]?.url
+                            },
+                            {
+                                name: 'index',
+                                type: 'ranges',
+                                definition: "1-" + Math.round(user.count * Object.values(usage)[0])
+                            }
+
+                        ]
+                    });
+                }
+
+            })
+        })
+
+        if (!fs.existsSync('./h0ck-converter-output')) {
+            fs.mkdirSync('./h0ck-converter-output');
         }
-        console.log("JSON data is saved.");
-    });
+        fs.writeFile('./h0ck-converter-output/outputJob-' + scenario + '.yaml', yaml.dump(jobResult), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON data is saved.");
+        });
 
-    fs.writeFile('./h0ck-output/outputJob-' + scenario + '.json', JSON.stringify(jobResult, null, 2), (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("JSON data is saved.");
-    });
+        fs.writeFile('./h0ck-converter-output/outputJob-' + scenario + '.json', JSON.stringify(jobResult, null, 2), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON data is saved.");
+        });
 
 
 
-})
+    })
+}
+
+
